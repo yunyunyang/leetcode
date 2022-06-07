8973c0f7ce0523857e0cb085d9fa1007988a7f12
Chloe <yunyunya@gmail.com>
2022-06-07 18:56:04 +0200

    test push

---
+++ src/main/java/com/leetcode/algorithms/easy/FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree.java
@@ -0,0 +1,14 @@
+package com.leetcode.algorithms.easy;
+
+public class FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree {
+
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        TreeNode node = null;
+
+
+        return node;
+    }
+
+    
+
+}
